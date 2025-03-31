fdb35575fe51b4cc9c2d7dcd0ad867ba3fed9b4f
DDbrother <derekyin42009@gmail.com>
2025-03-31 11:00:22 -0400

    feat: Vercel Analytics + Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import Header from "@/components/header"
 import Footer from "@/components/footer"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -20,6 +22,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics />
+      <SpeedInsights />
       <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
           <Header />
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+
+
 import { useEffect, useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
